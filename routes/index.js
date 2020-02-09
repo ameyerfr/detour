b6bc3cd116f410159900a42a6ed80be9d1866388
Alex Meyer <alexandre.meyer78@gmail.com>
2020-02-09 14:10:54 +0100

    Module loading & Alex testing pages

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,9 @@ router.get("/", (req, res) => {
   res.render("index");
 });
 
+router.get("/alex-test", (req, res) => {
+  res.render("alex-test", {modules:["alex"], gmapsk:process.env.GMAPS_KEY});
+});
+
+
 module.exports = router;
