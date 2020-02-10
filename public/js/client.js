7bdae9009ab3e79249a6cd5855fb1d9943bf1b72
abrebion <abrebion@gmail.com>
2020-02-10 15:53:06 +0100

    Expandable list

--- public/js/client.js
+++ public/js/client.js
@@ -19,15 +19,33 @@ const detour = new APIHandler("http://localhost:8000/api");
   function renderItem(data, target) {
     const itemTplHTML = `
         <a class="panel-block">
-        <span class="panel-icon">
-            <i class="fas fa-eye" aria-hidden="true"></i>
-        </span>
-        ${data.title}
+            <span class="panel-icon">
+                <i class="fas fa-eye" aria-hidden="true"></i>
+            </span>
+            <span class="poi-title">${data.title}</span>
         </a>
+        <div class="poi-details is-hidden is-size-7">
+            <div>
+                <p>${data.description}</p>
+                <p>${data.address}</p>
+                <p>Lat: ${data.coordinates.lat}, Lng: ${data.coordinates.lng}</p>
+            </div>
+            <a href="#" class="button is-small">Recompute Direction</a>
+        </div>
   `;
-    const itemTpl = document.createElement("template");
-    itemTpl.innerHTML = itemTplHTML;
-    const itemEl = itemTpl.content.cloneNode(true);
+    const itemEl = document.createElement("div");
+    itemEl.className = "poi-item";
+    itemEl.innerHTML = itemTplHTML;
     target.appendChild(itemEl);
+    itemEl.addEventListener("click", expandItem);
+  }
+
+  function expandItem(e) {
+    const allItems = document.querySelectorAll(".poi-details");
+    allItems.forEach(item => item.classList.add("is-hidden"));
+    e.target
+      .closest(".poi-item")
+      .querySelector(".poi-details")
+      .classList.remove("is-hidden");
   }
 })();
