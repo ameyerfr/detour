4f3876f0cbc86c2434af1a4fb872ef8e82f967a1
abrebion <abrebion@gmail.com>
2020-02-10 09:55:02 +0100

    Auth routes and views

---
+++ routes/auth.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/login", (req, res) => {
+  res.render("auth/login");
+});
+
+router.get("/register", (req, res) => {
+  res.render("auth/register");
+});
+
+module.exports = router;
