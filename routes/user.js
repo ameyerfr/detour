1654e6fd3d7d2163f46061a33f1325383cd7e149
Pascal Besson <pascalbes@gmail.com>
2020-02-10 10:15:38 +0100

    museumsfinal updated (lng lat)

--- routes/user.js
+++ routes/user.js
@@ -3,3 +3,51 @@ const router = express.Router();
 const userModel = require("../models/User.model");
 const poiModel = require("../models/Poi.model");
 const bcryptjs = require("bcryptjs");
+
+// TODO
+// /user/profile ( update password, preferences, deleted account) 
+// /user/poi/all ( show all the user's personnal pois ) 
+// /user/poi/new 
+// /user/poi/edit/:id
+
+
+router.get("/profil", (req, res) => {
+    res.render("user/profil");
+  });
+  
+
+//profil update : password 
+router.post("/profil/password/:id", (req, res) => {
+
+}
+
+//profil update : data (preferences) 
+router.post("/profil/data/:id", (req, res) => {
+
+    const {
+        preferences
+    } = req.body;
+
+    userModel
+        .findByIdAndUpdate(req.params.id, {
+            preferences
+        })
+        .then(() => {
+            req.flash("success", "sneaker successfully added");
+            res.redirect("/dashboard")
+        })
+        .catch(next);
+}
+
+
+//profil delete
+router.post("/profil/delete/:id", (req, res) => {
+    userModel
+    .findByIdAndDelete(req.params.id)
+        .then(dbRes => {
+            console.log("User account deleted", dbRes);
+            res.redirect("/dashboard");
+        })
+        .catch(dbErr => console.log("error deleting user", dbErr));
+}
+
