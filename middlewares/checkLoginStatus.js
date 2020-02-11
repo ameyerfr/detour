d705c1e0def201c0de4dc976c48668cc97223a45
abrebion <abrebion@gmail.com>
2020-02-11 10:55:00 +0100

    List cards

--- middlewares/checkLoginStatus.js
+++ middlewares/checkLoginStatus.js
@@ -1,19 +1,14 @@
 module.exports = function checkLoginStatus(req, res, next) {
-
-    res.locals.isLoggedIn = Boolean(req.session.currentUser);
-    if (res.locals.isLoggedIn) {
-        if (req.params.id == req.session.currentUser._id) {
-            res.locals.user_id=req.session.currentUser._id;
-            res.locals.message.error = req.flash("success", "Welcome on your POI page !");
-            next();
-        }
-        else {
-             res.locals.message.error = req.flash("error", "you don't have access to this page");
-        }
+  res.locals.isLoggedIn = Boolean(req.session.currentUser);
+  if (res.locals.isLoggedIn) {
+    if (req.params.id == req.session.currentUser._id) {
+      res.locals.user_id = req.session.currentUser._id;
+      res.locals.message_success = req.flash("success", "Welcome on your POI page !");
+      next();
+    } else {
+      res.locals.message_error = req.flash("error", "you don't have access to this page");
     }
-    else {
-        res.redirect("/login");
-    }
-
+  } else {
+    res.redirect("/login");
   }
-  
+};
