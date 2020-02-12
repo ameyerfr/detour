dfa1d6e1db06991b85678e5a0d02d7519ab5dab6
abrebion <abrebion@gmail.com>
2020-02-12 09:07:38 +0100

    backgroound image

--- middlewares/checkLoginStatus.js
+++ middlewares/checkLoginStatus.js
@@ -1,15 +1,16 @@
 module.exports = function checkLoginStatus(req, res, next) {
-
   res.locals.isLoggedIn = Boolean(req.session.currentUser);
+  console.log("----------", "is logged in?", res.locals.isLoggedIn);
+
   if (res.locals.isLoggedIn) {
     if (req.params.id == req.session.currentUser._id) {
       res.locals.user_id = req.session.currentUser._id;
       res.locals.message_success = req.flash("success", "Welcome on your POI page !");
-      next();
     } else {
       res.locals.message_error = req.flash("error", "you don't have access to this page");
     }
   } else {
     res.redirect("/login");
   }
+  next();
 };
