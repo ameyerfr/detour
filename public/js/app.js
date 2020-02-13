a2212a8c7780fd1065b6538144e3e11336234ce1
abrebion <abrebion@gmail.com>
2020-02-13 14:27:36 +0100

    More CSS for responsive site

--- public/js/app.js
+++ public/js/app.js
@@ -22,23 +22,4 @@ function initDetour(map) {
   initWithUrlParms();
 }
 
-// Misc functions required on all pages
-document.addEventListener("DOMContentLoaded", () => {
-  // Toggle main menu on mobile
-  const menu = document.querySelector(".navbar-burger");
-  menu.addEventListener("click", () => {
-    const target = menu.dataset.target;
-    const $target = document.getElementById(target);
-    menu.classList.toggle("is-active");
-    $target.classList.toggle("is-active");
-  });
-
-  // Remove notification messages
-  (document.querySelectorAll(".notification") || []).forEach($notification => {
-    $notification.addEventListener("click", () => {
-      $notification.remove();
-    });
-  });
-});
-
 initMap();
